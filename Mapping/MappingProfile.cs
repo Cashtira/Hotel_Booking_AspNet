1c2b0a31bec89b4a26a95fd4f5edc791c67b672b
khiemvuong <22520664@gm.uit.edu.vn>
2024-10-17 08:14:05 +0700

    khim vuong

---
+++ Mapping/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using MVCmodel.Models;
+using MVCmodel.DTOs;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Hotel, HotelDto>();
+        CreateMap<HotelDto, Hotel>();
+    }
+}
--- Models/Booking.cs
+++ Models/Booking.cs
@@ -1,15 +1,17 @@
 ﻿using MVCmodel.Models;
-
-public class Booking
+namespace MVCmodel.Models
 {
-    public int BookingID { get; set; }
-    public int GuestID { get; set; }
-    public int RoomID { get; set; }
+    public class Booking
+    {
+        public int BookingID { get; set; }
+        public int GuestID { get; set; }
+        public int RoomID { get; set; }
 
-    public required DateTime CheckinDate { get; set; }
-    public required DateTime CheckoutDate { get; set; }
+        public required DateTime CheckinDate { get; set; }
+        public required DateTime CheckoutDate { get; set; }
 
-    public double TotalPrice { get; set; }
-    public required Guest Guest { get; set; }
-    public required Room Room { get; set; }
+        public double TotalPrice { get; set; }
+        public required Guest Guest { get; set; }
+        public required Room Room { get; set; }
+    }
 }
