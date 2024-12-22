2618ef6341b29eb8d9c218959923acd94096bb19
22520565 <22520565@gm.uit.edu.vn>
2024-12-22 18:30:08 +0700

    Add interface services in "Application" project.

---
+++ 3. Application/Interfaces/Services/IRoleService.cs
@@ -0,0 +1,16 @@
+﻿namespace _3._Application.Interfaces.Services;
+
+using _3._Application.DTOs;
+
+public interface IRoleService
+{
+    Task<List<RoleDTO>> GetAllRolesAsync();
+
+    Task<RoleDTO?> GetRoleByIdAsync(int roleId);
+
+    Task AddRoleAsync(RoleDTO roleDto);
+
+    Task UpdateRoleAsync(RoleDTO roleDto);
+
+    Task DeleteRoleAsync(int roleId);
+}
--- 3. Application/Interfaces/Services/IUserService.cs
+++ 3. Application/Interfaces/Services/IUserService.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace _3._Application.Interfaces.Services;
 
-namespace _3._Application.Interfaces.Services
+using _3._Application.DTOs;
+
+public interface IUserService
 {
-    internal interface IUserService
-    {
-    }
+    Task<List<UserDTO>> GetAllUsersAsync();
+
+    Task<UserDTO?> GetUserByIdAsync(int userId);
+
+    Task AddUserAsync(UserDTO userDto);
+
+    Task UpdateUserAsync(UserDTO userDto);
+
+    Task DeleteUserAsync(int userId);
 }
