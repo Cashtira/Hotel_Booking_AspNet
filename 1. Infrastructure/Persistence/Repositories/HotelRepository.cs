4a675e642d7923f5dbe775e173303b0946ed3c80
22520565 <22520565@gm.uit.edu.vn>
2024-12-24 23:34:05 +0700

    Add Persistence Repositories in "Infrastructure" project.

---
+++ 1. Infrastructure/Persistence/Repositories/HotelRepository.cs
@@ -0,0 +1,43 @@
+﻿namespace _1._Infrastructure.Persistence.Repositories;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using _2._Domain.Entities;
+using _2._Domain.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+public class HotelRepository(ApplicationDbContext dbContext) : IHotelRepository
+{
+    private readonly ApplicationDbContext dbContext = dbContext ?? new ApplicationDbContext();
+
+    public async Task<IEnumerable<Hotel>> GetAllHotelsAsync()
+    {
+        return await this.dbContext.Hotels.ToListAsync().ConfigureAwait(true);
+    }
+
+    public async Task<Hotel?> GetHotelByIdAsync(int hotelId)
+    {
+        return await this.dbContext.Hotels.FirstOrDefaultAsync(e => e.HotelId == hotelId).ConfigureAwait(true);
+    }
+
+    public async Task AddHotelAsync(Hotel hotel)
+    {
+        this.dbContext.Add(hotel);
+        await this.dbContext.SaveChangesAsync().ConfigureAwait(true);
+    }
+    public async Task UpdateHotelAsync(Hotel hotel)
+    {
+        this.dbContext.Update(hotel);
+        await this.dbContext.SaveChangesAsync().ConfigureAwait(true);
+    }
+
+    public async Task DeleteHotelByIdAsync(int hotelId)
+    {
+        var hotel = await this.GetHotelByIdAsync(hotelId).ConfigureAwait(true);
+        if (hotel != null)
+        {
+            this.dbContext.Remove(hotel);
+            await this.dbContext.SaveChangesAsync().ConfigureAwait(true);
+        }
+    }
+}
