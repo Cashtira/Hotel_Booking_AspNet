c609ef1f15a05811f5841c8d6527c04cf352f1ef
22520565 <22520565@gm.uit.edu.vn>
2024-12-24 23:25:48 +0700

    Add Interface Repositories in "Domain" project.

--- 2. Domain/Interfaces/Repositories/IHotelRepository.cs
+++ 2. Domain/Interfaces/Repositories/IHotelRepository.cs
@@ -1,13 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace _2._Domain.Interfaces.Repositories;
 
-namespace _2._Domain.Interfaces.Repositories
+using _2._Domain.Entities;
+
+public interface IHotelRepository
 {
-    
-    interface IHotelRepository
-    {
-    }
+    Task<IEnumerable<Hotel>> GetAllHotelsAsync();
+
+    Task<Hotel?> GetHotelByIdAsync(int hotelId);
+
+    Task AddHotelAsync(Hotel hotel);
+
+    Task UpdateHotelAsync(Hotel hotel);
+
+    Task DeleteHotelByIdAsync(int hotelId);
 }
--- 2. Domain/Interfaces/Repositories/IUserRepository.cs
+++ 2. Domain/Interfaces/Repositories/IUserRepository.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace _2._Domain.Interfaces.Repositories;
 
-namespace _2._Domain.Interfaces.Repositories
+using _2._Domain.Entities;
+
+public interface IUserRepository
 {
-    public interface IUserRepository
-    {
-    }
+    Task<IEnumerable<User>> GetAllUsersAsync();
+
+    Task<User?> GetUserByIdAsync(int userId);
+
+    Task AddUserAsync(User user);
+
+    Task UpdateUserAsync(User user);
+
+    Task DeleteUserByIdAsync(int userId);
 }
