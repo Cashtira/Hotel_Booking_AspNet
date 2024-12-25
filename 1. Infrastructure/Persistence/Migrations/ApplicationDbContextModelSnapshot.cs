c25371ba6d74dcff0883b48cc5ddca6606dbb53a
22520565 <22520565@gm.uit.edu.vn>
2024-12-25 14:54:07 +0700

    Add "ApplicationDbContextModelSnapshot" in "Infrastructure" project.

---
+++ 1. Infrastructure/Persistence/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -0,0 +1,13 @@
+﻿namespace _1._Infrastructure.Persistence.Migrations;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+[DbContext(typeof(ApplicationDbContext))]
+internal sealed class ApplicationDbContextModelSnapshot : ModelSnapshot
+{
+    protected override void BuildModel(ModelBuilder modelBuilder)
+    {
+        ArgumentNullException.ThrowIfNull(modelBuilder);
+    }
+}
